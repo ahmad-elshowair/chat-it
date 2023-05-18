eb06424776375cd4cc6b4cd875b10ffcc3b43250
Ahmad Elshowair <it.is.ahmad.elshowair@outlook.com>
2023-05-18 21:10:48 +0700

    add the script of run the server on nodemon

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,4 +1,4 @@
-import express, { Application } from "express";
+import express, { Application, Request, Response } from "express";
 import config from "./configs/config";
 
 // create an instance of app
@@ -8,11 +8,11 @@ const app: Application = express();
 const port: number = config.port;
 
 // create a get request of home endpoint
-app.get("/", (req, res) => {
+app.get("/", (_req: Request, res: Response) => {
 	res.send("Hello World!");
 });
 
 // add listen to the app
 app.listen(port, () => {
-	console.log(`Server is running on port: ${port}`);
+	console.log(`Server is running on http://localhost:${port}`);
 });
