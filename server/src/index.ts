e61643c78ab7fd77087877ff4b43d4baa9d5bcaa
Ahmad Elshowair <it.is.ahmad.elshowair@outlook.com>
2023-05-20 13:25:34 +0700

    add middlewares

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,4 +1,7 @@
+import cors from "cors";
 import express, { Application, Request, Response } from "express";
+import helmet from "helmet";
+import morgan from "morgan";
 import config from "./configs/config";
 
 // create an instance of app
@@ -6,6 +9,15 @@ const app: Application = express();
 
 // create port variable that is assigned from the config file
 const port: number = config.port;
+
+// use the middleware of express.json and helmet and morgan
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(helmet());
+app.use(morgan("dev"));
+
+// use the cors
+app.use(cors());
 
 // create a get request of home endpoint
 app.get("/", (_req: Request, res: Response) => {
