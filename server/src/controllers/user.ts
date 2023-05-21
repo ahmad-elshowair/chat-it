d60c39bf3f1f30608302939552adca41d53b812f
Ahmad Elshowair <it.is.ahmad.elshowair@outlook.com>
2023-05-21 15:48:11 +0700

    add create a user model & controller method

--- server/src/controllers/user.ts
+++ server/src/controllers/user.ts
@@ -12,6 +12,15 @@ const getUsers = async (req: Request, res: Response) => {
 	}
 };
 
+const createUser = async (req: Request, res: Response) => {
+	try {
+		const user = await user_model.create(req.body);
+		res.status(201).json({ ...user });
+	} catch (error) {
+		res.status(400).json({ error: (error as Error).message });
+	}
+};
+
 export default {
 	getUsers,
 };
--- server/src/models/user.ts
+++ server/src/models/user.ts
@@ -1,4 +1,8 @@
+import bcrypt from "bcrypt";
+import { QueryResult } from "pg";
+import config from "../configs/config";
 import db from "../database/pool";
+import User from "../types/users";
 
 class UserModel {
 	// get all users
@@ -23,6 +27,52 @@ class UserModel {
 			};
 		} catch (error) {
 			console.error(`cannot get all users due to ${(error as Error).message}`);
+		} finally {
+			// release the connection
+			connection.release();
+		}
+	}
+
+	// a method to hash the inputting password
+	hashPassword(password: string) {
+		return bcrypt.hashSync(password + config.pepper, config.salt);
+	}
+
+	//a method to create a new user
+	async create(user: User) {
+		// connect to the database
+		const connection = await db.connect();
+		try {
+			// check ig the user exist
+			const checkUserQuery = "SELECT * FROM users WHERE user_email=$1";
+			const checkUser: QueryResult<User> = await db.query(checkUserQuery, [
+				user.user_email,
+			]);
+			if (checkUser.rowCount > 0) {
+				return {
+					error: true,
+					message: "User already exist",
+				};
+			}
+
+			// insert the new user
+			const insertUserQuery =
+				"INSERT INTO users (user_name, user_email, user_password,) VALUES ($1, $2, $3) RETURNING *";
+			const insertUser = await db.query(insertUserQuery, [
+				user.user_name,
+				user.user_email,
+				this.hashPassword(user.user_password),
+			]);
+			// return the inserted user
+			return {
+				error: false,
+				message: "User created",
+				user: insertUser.rows[0],
+			};
+		} catch (error) {
+			console.error(
+				`cannot create a new user due to ${(error as Error).message}`,
+			);
 		} finally {
 			// release the connection
 			connection.release();
