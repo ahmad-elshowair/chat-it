2b6f1715327b54dcc895d14c8965dfea81315da9
Ahmad Elshowair <it.is.ahmad.elshowair@outlook.com>
2023-05-21 13:20:08 +0700

    declare the type of the salt round env variable

--- server/src/configs/config.ts
+++ server/src/configs/config.ts
@@ -1,15 +1,15 @@
-import dotenv from 'dotenv'
+import dotenv from "dotenv";
 
 dotenv.config();
 
 export default {
-    port: Number(process.env.PORT),
-    pg_port: Number(process.env.PG_PORT),
-    pg_host: process.env.PG_HOST,
-    pg_user: process.env.PG_USER,
-    pg_password: process.env.PG_PASSWORD,
-    pg_database: process.env.PG_DATABASE,
-    jwt_secret: process.env.JWT_SECRET,
-    salt: process.env.SALT_ROUNDS,
-    pepper: process.env.PEPPER 
-}
+	port: Number(process.env.PORT),
+	pg_port: Number(process.env.PG_PORT),
+	pg_host: process.env.PG_HOST,
+	pg_user: process.env.PG_USER,
+	pg_password: process.env.PG_PASSWORD,
+	pg_database: process.env.PG_DATABASE,
+	jwt_secret: process.env.JWT_SECRET,
+	salt: Number(process.env.SALT_ROUNDS),
+	pepper: process.env.PEPPER,
+};
