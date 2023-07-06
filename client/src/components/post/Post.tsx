930f5ece3961ced052eafefedf173239d90ab49e
Ahmad Elshowair <it.is.ahmad.elshowair@outlook.com>
2023-07-06 16:27:08 +0300

    style the header of the post component

---
+++ client/src/components/post/Post.tsx
@@ -0,0 +1,50 @@
+import "./post.css";
+
+import { FaEllipsisH, FaGlobeEurope, FaTimes } from "react-icons/fa";
+export const Post = () => {
+  const date: Date = new Date();
+  const dateFormate = date.toLocaleString("en-GB", {
+    day: "numeric",
+    month: "long",
+    hour: "numeric",
+    minute: "numeric",
+    hour12: false,
+  });
+  return (
+    <section className="post card mt-3">
+      <div className="card-body">
+        <article className="post-header">
+          <div className="post-header-info">
+            <figure>
+              <a href="#profile">
+                <img
+                  className="post-header-img-user"
+                  src="/assets/avatars/1.jpeg"
+                  alt="profile"
+                />
+              </a>
+            </figure>
+            <div className="post-header-info-links">
+              <a
+                className="post-header-info-links-user"
+                href="#profile"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                user
+              </a>
+              <a href="#profile" className="post-header-info-links-date">
+                {dateFormate}
+              </a>
+              <FaGlobeEurope className="post-header-info-links-icon" />
+            </div>
+          </div>
+          <div className="post-option-bars">
+            <FaEllipsisH className="post-option-bars-icon" />
+            <FaTimes className="post-option-bars-icon" />
+          </div>
+        </article>
+      </div>
+    </section>
+  );
+};
