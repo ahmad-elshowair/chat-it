966ab3963e52c13b79c75334ebcbacebf9f9de7e
Ahmad Elshowair <it.is.ahmad.elshowair@outlook.com>
2024-06-07 18:52:30 +0700

    rename the column description to bio

--- server/migrations/sqls/20230520052909-users-up.sql
+++ server/migrations/sqls/20230520052909-users-up.sql
@@ -4,18 +4,17 @@ CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 
 -- create a users table if not exists
 
-CREATE TABLE
-    users (
-        user_id uuid DEFAULT uuid_generate_v4() PRIMARY KEY,
-        user_name VARCHAR NOT NULL,
-        email VARCHAR NOT NULL UNIQUE,
-        password VARCHAR NOT NULL,
-        picture VARCHAR DEFAULT '',
-        cover VARCHAR DEFAULT '',
-        is_admin BOOLEAN DEFAULT FALSE,
-        description VARCHAR DEFAULT '',
-        city VARCHAR(50) DEFAULT '',
-        home_town VARCHAR(50) DEFAULT '',
-        created_at TIMESTAMP DEFAULT NOW(),
-        updated_at TIMESTAMP DEFAULT NOW()
-    );
+CREATE TABLE users (
+    user_id uuid DEFAULT uuid_generate_v4 () PRIMARY KEY,
+    user_name VARCHAR NOT NULL,
+    email VARCHAR NOT NULL UNIQUE,
+    password VARCHAR NOT NULL,
+    picture VARCHAR DEFAULT '',
+    cover VARCHAR DEFAULT '',
+    is_admin BOOLEAN DEFAULT FALSE,
+    description VARCHAR DEFAULT '',
+    city VARCHAR(50) DEFAULT '',
+    home_town VARCHAR(50) DEFAULT '',
+    created_at TIMESTAMP DEFAULT NOW(),
+    updated_at TIMESTAMP DEFAULT NOW()
+);
---
+++ server/migrations/sqls/20240604135532-rename-description-column-down.sql
@@ -0,0 +1,1 @@
+ALTER TABLE users RENAME bio TO description;
