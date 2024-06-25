af4c9daf5ce10b8dc57b4ac7e7700bc9ba316f08
Ahmad Elshowair <it.is.ahmad.elshowair@outlook.com>
2024-06-25 17:58:11 +0700

    add a helper function to build dynamic update query

---
+++ server/src/utilities/build-update-query.ts
@@ -0,0 +1,27 @@
+import User from "../types/users";
+import passwords from "./passwords";
+
+export const buildUpdateQuery = (
+	id: string,
+	updates: Partial<User>,
+): (string | (string | boolean | Date)[])[] => {
+	const fields = [];
+	const values: (string | boolean | Date)[] = [];
+	let index = 1;
+
+	for (const [key, value] of Object.entries(updates)) {
+		if (key === "password") {
+			fields.push(`${key} = $${index}`);
+			values.push(passwords.hashPassword(value as string));
+		} else {
+			fields.push(`${key} = $${index}`);
+			values.push(value);
+		}
+		index++;
+	}
+	values.push(id);
+	const query = `UPDATE users SET ${fields.join(
+		", ",
+	)} WHERE user_id = $${index} RETURNING *`;
+	return [query, values];
+};
