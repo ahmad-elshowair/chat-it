9608548ffb4147e92d4cf59b1573448a2956f696
Ahmad Elshowair <it.is.ahmad.elshowair@outlook.com>
2024-06-27 18:49:21 +0700

    "Added react-router-dom dependency and implemented routing in App.tsx and index.tsx"

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,14 +1,19 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap";
+import { Route, Routes } from "react-router-dom";
 import "./App.css";
+import { Home } from "./pages/home/Home";
 import { Login } from "./pages/loginPage/Login";
 
 function App() {
-  return (
-    <>
-      <Login />
-    </>
-  );
+	return (
+		<>
+			<Routes>
+				<Route path="/" element={<Home />} />
+				<Route path="/login" element={<Login />} />
+			</Routes>
+		</>
+	);
 }
 
 export default App;
