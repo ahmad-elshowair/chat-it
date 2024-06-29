983b52259eba40f4a17a35f080557308fe8745c6
Ahmad Elshowair <it.is.ahmad.elshowair@outlook.com>
2024-06-29 18:01:14 +0700

    "No changes made, identical code reformatted."

--- server/src/controllers/post.ts
+++ server/src/controllers/post.ts
@@ -8,99 +8,99 @@ const postService = new PostModel();
 
 // create a post
 const create = async (req: CustomRequest, res: Response) => {
-  try {
-    const post: Post = {
-      user_id: req.user.id,
-      description: req.body.description,
-      image: req.body.image,
-    };
-    const createPost = await postService.create(post);
-    res.status(201).json(createPost);
-  } catch (error) {
-    res.status(400).json({ error: (error as Error).message });
-  }
+	try {
+		const post: Post = {
+			user_id: req.user.id,
+			description: req.body.description,
+			image: req.body.image,
+		};
+		const createPost = await postService.create(post);
+		res.status(201).json(createPost);
+	} catch (error) {
+		res.status(400).json({ error: (error as Error).message });
+	}
 };
 // update a post
 const update = async (req: CustomRequest, res: Response) => {
-  try {
-    // check if the user is the owner of this post
-    const post = req.body;
-    if (req.user.id === post.user_id) {
-      const updatedPost = await postService.update(req.params.id, post);
-      res.status(200).json(updatedPost);
-    } else {
-      res.status(401).json({ message: "THAT IS NOT YOUR POST !" });
-    }
-  } catch (error) {
-    res.status(400).json({ error: (error as Error).message });
-  }
+	try {
+		// check if the user is the owner of this post
+		const post = req.body;
+		if (req.user.id === post.user_id) {
+			const updatedPost = await postService.update(req.params.id, post);
+			res.status(200).json(updatedPost);
+		} else {
+			res.status(401).json({ message: "THAT IS NOT YOUR POST !" });
+		}
+	} catch (error) {
+		res.status(400).json({ error: (error as Error).message });
+	}
 };
 
 // get post by id
 const getPostById = async (req: Request, res: Response) => {
-  try {
-    const post = await postService.getById(req.params.id);
-    res.status(200).json(post);
-  } catch (error) {
-    res.status(400).json({ error: (error as Error).message });
-  }
+	try {
+		const post = await postService.getById(req.params.id);
+		res.status(200).json(post);
+	} catch (error) {
+		res.status(400).json({ error: (error as Error).message });
+	}
 };
 
 // get all posts
 const getAllPosts = async (req: Request, res: Response) => {
-  try {
-    const posts = await postService.getAll();
-    res.status(200).json(posts);
-  } catch (error) {
-    res.status(400).json({ error: (error as Error).message });
-  }
+	try {
+		const posts = await postService.getAll();
+		res.status(200).json(posts);
+	} catch (error) {
+		res.status(400).json({ error: (error as Error).message });
+	}
 };
 
 // delete post
 const deletePost = async (req: CustomRequest, res: Response) => {
-  try {
-    const post = await postService.getById(req.params.id);
-    if (post.user_id === req.user.id) {
-      const deletePost = await postService.delete(req.params.id);
-      res.status(200).json(deletePost);
-    } else {
-      res.status(401).json({ message: "THAT IS NOT YOUR POST !" });
-    }
-  } catch (error) {
-    res.status(400).json({ error: (error as Error).message });
-  }
+	try {
+		const post = await postService.getById(req.params.id);
+		if (post.user_id === req.user.id) {
+			const deletePost = await postService.delete(req.params.id);
+			res.status(200).json(deletePost);
+		} else {
+			res.status(401).json({ message: "THAT IS NOT YOUR POST !" });
+		}
+	} catch (error) {
+		res.status(400).json({ error: (error as Error).message });
+	}
 };
 // GET ALL POSTS BY USER ID
 const getAllByUserId = async (req: CustomRequest, res: Response) => {
-  try {
-    const posts: Post[] = await postService.getAllByUserId(req.user.id);
-    res.status(200).json(posts);
-  } catch (error) {
-    res.status(400).json({ error: (error as Error).message });
-  }
+	try {
+		const posts: Post[] = await postService.getAllByUserId(req.user.id);
+		res.status(200).json(posts);
+	} catch (error) {
+		res.status(400).json({ error: (error as Error).message });
+	}
 };
 
 // GET ALL POSTS OF A USER AND HIS FOLLOWINGS
 const getAllByUserIdAndFollowings = async (
-  req: CustomRequest,
-  res: Response
+	req: CustomRequest,
+	res: Response,
 ) => {
-  try {
-    const posts: Post[] = await postService.getAllByUserIdAndFollowing(
-      req.user.id
-    );
-    res.status(200).json(posts);
-  } catch (error) {
-    res.status(400).json({ error: (error as Error).message });
-  }
+	try {
+		const posts: Post[] = await postService.getAllByUserIdAndFollowing(
+			req.user.id,
+		);
+		res.status(200).json(posts);
+	} catch (error) {
+		res.status(400).json({ error: (error as Error).message });
+	}
 };
 
 export default {
-  create,
-  update,
-  getPostById,
-  getAllPosts,
-  deletePost,
-  getAllByUserId,
-  getAllByUserIdAndFollowings,
+	create,
+	update,
+	getPostById,
+	getAllPosts,
+	deletePost,
+	getAllByUserId,
+	getAllByUserIdAndFollowings,
 };
