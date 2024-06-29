4ce8a01267fae0d8710995f6b4db28c9950383c5
Ahmad Elshowair <it.is.ahmad.elshowair@outlook.com>
2024-06-29 14:40:32 +0700

    "Updated App.css with new btn-chat styles, and modified Login.tsx and login.css to add required attribute to input fields, update button class, and adjust login form styles."

--- client/src/pages/login/Login.tsx
+++ client/src/pages/login/Login.tsx
@@ -1,41 +1,43 @@
 import "./login.css";
 export const Login = () => {
-  return (
-    <section className="login-page">
-      <form action="" className="login-form">
-        <header className="login-form-header">
-          <img src="/assets/chat-it.png" alt="logo" />
-        </header>
-        <div className="login-form-body">
-          <div className="login-form-body-input">
-            <label className="form-label" htmlFor="email">
-              Email
-            </label>
-            <input
-              className="form-control"
-              type="email"
-              name="email"
-              id="email"
-              placeholder="exmaple@gmail.com"
-            />
-          </div>
-          <div className="login-form-body-input">
-            <label className="form-label" htmlFor="password">
-              Password
-            </label>
-            <input
-              className="form-control"
-              type="password"
-              name="password"
-              id="password"
-              placeholder="your password!"
-            />
-          </div>
-          <button className="btn" type="submit">
-            Login
-          </button>
-        </div>
-      </form>
-    </section>
-  );
+	return (
+		<section className="login-page">
+			<form action="" className="login-form">
+				<header className="login-form-header">
+					<img src="/assets/chat-it.png" alt="logo" />
+				</header>
+				<div className="login-form-body">
+					<div className="login-form-body-input">
+						<label className="form-label" htmlFor="email">
+							Email
+						</label>
+						<input
+							className="form-control"
+							type="email"
+							name="email"
+							id="email"
+							placeholder="exmaple@gmail.com"
+							required
+						/>
+					</div>
+					<div className="login-form-body-input">
+						<label className="form-label" htmlFor="password">
+							Password
+						</label>
+						<input
+							className="form-control"
+							type="password"
+							name="password"
+							id="password"
+							placeholder="***********"
+							required
+						/>
+					</div>
+					<button className="btn btn-chat" type="submit">
+						Login
+					</button>
+				</div>
+			</form>
+		</section>
+	);
 };
