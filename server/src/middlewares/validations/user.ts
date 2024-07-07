b6f8d25b1996b9bd58d6f48a4f718a67dcc594aa
Ahmad Elshowair <it.is.ahmad.elshowair@outlook.com>
2024-07-07 15:59:46 +0700

    "Refactored user validation middleware to add new validation rules and improve password checking logic."

--- server/src/middlewares/validations/user.ts
+++ server/src/middlewares/validations/user.ts
@@ -2,44 +2,105 @@ import { ValidationChain, check } from "express-validator";
 import db from "../../database/pool";
 import passwords from "../../utilities/passwords";
 
+const checkLength = (
+	field: string,
+	min: number,
+	message: string,
+): ValidationChain => check(field).isLength({ min }).withMessage(message);
+
 const checkIsEmail: ValidationChain = check("email")
 	.isEmail()
 	.withMessage("Invalid email format! Example: 'example@domain-name.com'");
 
-const checkPasswordLength: ValidationChain = check("password")
-	.isLength({ min: 6 })
-	.withMessage("PASSWORD MUST BE AT LEAST 6 CHARACTERS LONG !");
+const checkPasswordLength: ValidationChain = checkLength(
+	"password",
+	6,
+	"PASSWORD MUST BE AT LEAST 6 CHARACTERS LONG !",
+);
+const checkPasswordMatch: ValidationChain = check("confirm_password").custom(
+	(confirmPassword: string, { req }) => {
+		if (confirmPassword !== req.body.password) {
+			throw new Error(`PASSWORDS DO NOT MATCH !`);
+		}
+		return true;
+	},
+);
 
-const checkName: ValidationChain = check("user_name")
-	.isLength({ min: 3 })
-	.withMessage("USERNAME MUST BE AT LEAST 6 CHARACTERS LONG !");
+// CHECK IF THE PASSWORD IS CORRECT.
+const checkPassword: ValidationChain = check("password").custom(
+	async (password: string, { req }) => {
+		const connection = await db.connect();
+		try {
+			const result = await connection.query(
+				"SELECT * FROM users WHERE email = $1",
+				[req.body.email],
+			);
+			const user = result.rows[0];
+			// check if the password is correct
+			if (!user || !passwords.checkPassword(password, user.rows[0].password)) {
+				throw new Error("INCORRECT PASSWORD !");
+			}
+		} catch (error) {
+			throw new Error(`Authentication failed ${(error as Error).message}!`);
+		} finally {
+			connection.release();
+		}
+	},
+);
 
-const checkLogin: ValidationChain = check("email").custom(
+// CHECK IF THE USER NAME LENGTH IS AT LEAST 3 CHARACTERS.
+const checkNameLength: ValidationChain = checkLength(
+	"user_name",
+	3,
+	"USERNAME MUST BE AT LEAST 6 CHARACTERS LONG !",
+);
+
+// CHECK IF THE FIRST NAME LENGTH IS AT LEAST 3 CHARACTER
+const checkFirstNameLength: ValidationChain = checkLength(
+	"first_name",
+	3,
+	"THE FIRST NAME MUST BE AT LEAST 3 CHARACTERS!",
+);
+
+// CHECK IF THE FIRST NAME LENGTH IS AT LEAST 3 CHARACTER
+const checkLastNameLength: ValidationChain = checkLength(
+	"last_name",
+	3,
+	"THE LAST NAME MUST BE AT LEAST 3 CHARACTERS!",
+);
+
+//CHECK IF THE EMAIL EXISTS IN THE DATABASE.
+const isEmailExist: ValidationChain = check("email").custom(
 	async (email: string, { req }) => {
 		// connect to the database
 		const connection = await db.connect();
 		try {
 			// select the user from the database
-			const user = await db.query("SELECT * FROM users WHERE email=$1", [
+			const result = await db.query("SELECT * FROM users WHERE email=$1", [
 				email,
 			]);
-			// check if the email exist
-			if (user.rows.length === 0) {
-				throw new Error(`${email} IS NOT EXIST !`);
+			const user = result.rows[0];
+
+			if (!user) {
+				throw new Error(`${email} DOES NOT EXIST !`);
 			}
-			// check if the password is correct
-			if (!passwords.checkPassword(req.body.password, user.rows[0].password)) {
-				throw new Error("INCORRECT PASSWORD !");
-			}
-			req.user = user.rows[0];
+
+			req.user = user;
 		} catch (error) {
-			throw new Error((error as Error).message);
+			throw new Error(`USER LOOKUP FAILED: ${(error as Error).message}`);
 		} finally {
 			connection.release();
 		}
 	},
 );
 export default {
-	register: [checkIsEmail, checkPasswordLength, checkName],
-	login: [checkLogin, checkIsEmail],
+	register: [
+		checkIsEmail,
+		checkPasswordLength,
+		checkNameLength,
+		checkFirstNameLength,
+		checkLastNameLength,
+		checkPasswordMatch,
+	],
+	login: [isEmailExist, checkIsEmail, checkPasswordLength, checkPassword],
 };
