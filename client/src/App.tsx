3d39c5f3c76197b5354718bcc9c7bcf78cac8a1b
Ahmad Elshowair <it.is.ahmad.elshowair@outlook.com>
2024-09-01 16:30:16 +0700

    "Moved @types/bootstrap from devDependencies to dependencies in package.json, updated App.tsx to use useEffect and setUpInterceptors, and refactored axiosInstance.ts to use Dispatch and AuthState"

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,12 +1,13 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap";
-import { useContext } from "react";
+import { useContext, useEffect } from "react";
 import {
 	Navigate,
 	Route,
 	BrowserRouter as Router,
 	Routes,
 } from "react-router-dom";
+import { setUpInterceptors } from "./api/axiosInstance";
 import "./App.css";
 import { AuthContext } from "./context/AuthContext";
 import { Home } from "./pages/home/Home";
@@ -15,25 +16,29 @@ import { Profile } from "./pages/profile/Profile";
 import { Register } from "./pages/register/Register";
 
 function App() {
-	const { user } = useContext(AuthContext).state;
+	const { state, dispatch } = useContext(AuthContext);
+	useEffect(() => {
+		setUpInterceptors(state, dispatch);
+	}, [state, dispatch]);
+
 	return (
 		<Router>
 			<Routes>
-				<Route path="/" element={user ? <Home /> : <Login />} />
+				<Route path="/" element={state.user ? <Home /> : <Login />} />
 
 				<Route
 					path="/login"
-					element={user ? <Navigate to="/" replace /> : <Login />}
+					element={state.user ? <Navigate to="/" replace /> : <Login />}
 				/>
 
 				<Route
 					path="/profile/:user_name"
-					element={user ? <Profile /> : <Navigate to="/login" replace />}
+					element={state.user ? <Profile /> : <Navigate to="/login" replace />}
 				/>
 
 				<Route
 					path="/register"
-					element={user ? <Navigate to="/" replace /> : <Register />}
+					element={state.user ? <Navigate to="/" replace /> : <Register />}
 				/>
 			</Routes>
 		</Router>
--- client/src/api/axiosInstance.ts
+++ client/src/api/axiosInstance.ts
@@ -1,48 +1,52 @@
 import axios from "axios";
-import { useContext } from "react";
+import { Dispatch } from "react";
 import config from "../configs";
-import { AuthContext } from "../context/AuthContext";
+import { AuthAction, AuthState } from "../types";
 
 const api = axios.create({
 	baseURL: config.api_url,
 	withCredentials: true,
 });
 
-api.interceptors.request.use(
-	(config) => {
-		const { user } = useContext(AuthContext).state;
-		if (user?.access_token) {
-			config.headers.Authorization = `Bearer ${user.access_token}`;
-		}
-		return config;
-	},
-	(error) => Promise.reject(error),
-);
-
-api.interceptors.response.use(
-	(response) => response,
-	async (error) => {
-		const { state, dispatch } = useContext(AuthContext);
-		const originalRequest = error.config;
-		if (error.response.status === 401 && !originalRequest._retry) {
-			originalRequest._retry = true;
-			try {
-				const { data } = await axios.post(
-					`${config.api_url}/auth/refresh-token`,
-					{ refresh_token: state.user?.refresh_token },
-				);
-				dispatch({
-					type: "REFRESH_TOKEN",
-					payload: { access_token: data.access_token },
-				});
-
-				originalRequest.headers.Authorization = `Bearer ${data.access_token}`;
-				return api(originalRequest);
-			} catch (error) {
-				return Promise.reject(error);
+export const setUpInterceptors = (
+	authState: AuthState,
+	dispatch: Dispatch<AuthAction>,
+) => {
+	api.interceptors.request.use(
+		(config) => {
+			if (authState.user?.access_token) {
+				config.headers.Authorization = `Bearer ${authState.user.access_token}`;
 			}
-		}
-		return Promise.reject(error);
-	},
-);
+			return config;
+		},
+		(error) => Promise.reject(error),
+	);
+
+	api.interceptors.response.use(
+		(response) => response,
+		async (error) => {
+			const originalRequest = error.config;
+			if (error.response.status === 401 && !originalRequest._retry) {
+				originalRequest._retry = true;
+				try {
+					const { data } = await axios.post(
+						`${config.api_url}/auth/refresh-token`,
+						{ refresh_token: authState.user?.refresh_token },
+					);
+					dispatch({
+						type: "REFRESH_TOKEN",
+						payload: { access_token: data.access_token },
+					});
+
+					originalRequest.headers.Authorization = `Bearer ${data.access_token}`;
+					return api(originalRequest);
+				} catch (error) {
+					dispatch({ type: "FAILURE", payload: [(error as Error).message] });
+					return Promise.reject(error);
+				}
+			}
+			return Promise.reject(error);
+		},
+	);
+};
 export default api;
