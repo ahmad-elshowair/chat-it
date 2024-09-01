6a9a2a8aae28f1bcf8ab030c172cb01d06c1255d
Ahmad Elshowair <it.is.ahmad.elshowair@outlook.com>
2024-09-01 11:16:12 +0700

    create refresh-token endpoint, that refresh the access token that is created with login or register endpoint, add logout end point

--- server/src/controllers/auth.ts
+++ server/src/controllers/auth.ts
@@ -1,9 +1,9 @@
 import { NextFunction, Request, Response } from "express";
+import jwt, { JwtPayload } from "jsonwebtoken";
 import config from "../configs/config";
 import { UserPayload } from "../interfaces/IUserPayload";
 import AuthModel from "../models/auth";
 import { generateToken } from "../utilities/generateToken";
-
 const user_model = new AuthModel();
 
 const register = async (req: Request, res: Response, next: NextFunction) => {
@@ -13,24 +13,30 @@ const register = async (req: Request, res: Response, next: NextFunction) => {
 			id: user.user_id,
 			is_admin: user.is_admin,
 		};
-		const token = generateToken(payload, config.jwt_secret);
+		// generate access token
+		const accessToken = generateToken(payload, config.jwt_secret, "15m");
+		// generate access token
+		const refreshToken = generateToken(
+			payload,
+			config.jwt_refresh_secret,
+			"7d",
+		);
 
-		res.status(201).json({
-			user_id: user.user_id,
-			user_name: user.user_name,
-			email: user.email,
-			picture: user.picture,
-			cover: user.cover,
-			is_admin: user.is_admin,
-			bio: user.bio,
-			city: user.city,
-			home_town: user.home_town,
-			updated_at: user.updated_at,
-			first_name: user.first_name,
-			last_name: user.last_name,
-			marital_status: user.marital_status,
-			token: token,
+		res.cookie("access_token", accessToken, {
+			httpOnly: true,
+			sameSite: "strict",
+			secure: true,
+			maxAge: 15 * 60 * 1000,
 		});
+
+		res.cookie("refresh_token", refreshToken, {
+			httpOnly: true,
+			sameSite: "strict",
+			secure: true,
+			maxAge: 7 * 24 * 60 * 60 * 1000,
+		});
+
+		res.status(201).json({ message: "Register Successfully", user });
 	} catch (error) {
 		next(error);
 	}
@@ -52,30 +58,71 @@ const login = async (
 		};
 
 		// generate access token
-		const token = generateToken(payload, config.jwt_secret);
+		const accessToken = generateToken(payload, config.jwt_secret, "15m");
+		// generate access token
+		const refreshToken = generateToken(payload, config.jwt_secret, "7d");
 
-		res.status(200).json({
-			user_id: user.user_id,
-			user_name: user.user_name,
-			email: user.email,
-			picture: user.picture,
-			cover: user.cover,
-			is_admin: user.is_admin,
-			bio: user.bio,
-			city: user.city,
-			home_town: user.home_town,
-			updated_at: user.updated_at,
-			first_name: user.first_name,
-			last_name: user.last_name,
-			marital_status: user.marital_status,
-			token: token,
+		res.cookie("access_token", accessToken, {
+			httpOnly: true,
+			sameSite: "strict",
+			secure: true,
+			maxAge: 15 * 60 * 1000,
 		});
+
+		res.cookie("refresh_token", refreshToken, {
+			httpOnly: true,
+			sameSite: "strict",
+			secure: true,
+			maxAge: 7 * 24 * 60 * 60 * 1000,
+		});
+
+		res.status(201).json({ message: "Login Successfully", user });
 	} catch (error) {
 		next(error);
 	}
 };
 
+// Refresh Token
+const refreshToken = async (req: Request, res: Response) => {
+	const refreshToken = req.cookies.refresh_token;
+	if (!refreshToken) {
+		return res.status(401).json({ message: "Refresh Token is Missing!" });
+	}
+	try {
+		const decoded = jwt.verify(
+			refreshToken,
+			config.jwt_refresh_secret,
+		) as JwtPayload;
+
+		if (!decoded) {
+			return res.status(403).json({ message: "token is invalid!" });
+		}
+
+		const payload: UserPayload = { id: decoded.id, is_admin: decoded.is_admin };
+
+		// Generate new Access Token.
+		const newAccessToken = generateToken(payload, config.jwt_secret, "15m");
+
+		res.cookie("access_token", newAccessToken, {
+			httpOnly: true,
+			sameSite: "strict",
+			secure: true,
+			maxAge: 15 * 60 * 1000, // 15 minutes
+		});
+		res.json({ message: "Access Token Refreshed" });
+	} catch (error) {
+		return res.status(403).json({ message: "Invalid Refresh Token!" });
+	}
+};
+
+const logout = (req: Request, res: Response) => {
+	res.clearCookie("access_token");
+	res.clearCookie("refresh_token");
+	res.status(200).json({ message: "Logout Successfully!" });
+};
 export default {
 	register,
 	login,
+	refreshToken,
+	logout,
 };
